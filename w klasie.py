473abbbf4125fc42bc7cf60b9e637096b6fd1f63
lukeSDZ <100701145+lukeSDZ@users.noreply.github.com>
2022-03-02 13:34:38 +0100

    Add files via upload

---
+++ w klasie.py
@@ -0,0 +1,21 @@
+class Colors():
+    def __init__(self, kolor):
+        self.kolor = kolor
+
+        #slownik z kolorami, teraz tylko 1 dla testow
+    _przykladowe_kolory = {
+        "Red" : "#e6194B"
+    }
+
+    def to_hex(self, kolor_key):
+        if kolor_key in self._przykladowe_kolory.keys():
+            for kolor_key, HEX in self._przykladowe_kolory:
+                print(HEX)
+        else: print("Nie ma w slowniku")
+
+
+czerwony = Colors("czerwony")
+print(czerwony)
+
+czerwony.to_hex(kolor_key="Red")
+ 
